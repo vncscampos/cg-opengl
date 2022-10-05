c04092834f795ad808059ef07c0ff9eaf431f9d4
viniolic <viniciuscampos@tokenlab.com.br>
2022-10-04 22:40:05 -0300

    atividade pratica 04

--- atv4/Projeto02/main.cpp
+++ atv4/Projeto02/main.cpp
@@ -1,21 +1,14 @@
-//*****************************************************
-//
-// CasaComTransRotEsc.cpp
-// Um programa OpenGL simples que abre uma janela GLUT,
-// translada, rotaciona e troca a escala de um casa para
-// depois desenha-la.
-//
-// Marcelo Cohen e Isabel H. Manssour
-// Este c�digo acompanha o livro
-// "OpenGL - Uma Abordagem Pr�tica e Objetiva"
-//
-//*****************************************************
-
 #ifdef __APPLE__
 #include <GLUT/glut.h>
 #else
 #include <GL/glut.h>
 #endif
+
+//vari�veis para o controle da transla��o
+GLfloat posicaoX=0, posicaoY=0;
+
+//vari�vel para o controle da escala nos eixos X e Y
+GLfloat escala=1;
 
 //vari�vel para o controle do angulo de rota��o no eixo Z
 GLfloat  angulo=0;
@@ -26,20 +19,20 @@ void Desenha(void)
 	// Muda para o sistema de coordenadas do modelo
 	glMatrixMode(GL_MODELVIEW);
 	// Inicializa a matriz de transforma��o corrente
-	glLoadIdentity();
+    glLoadIdentity();
 
 	// Limpa a janela de visualiza��o com a cor
 	// de fundo definida previamente
 	glClear(GL_COLOR_BUFFER_BIT);
 
 	// Aplica uma transla��o sobre a casinha que ser� desenhada
-	glTranslatef(15.0f, 0.0f, 0.0f);
+	glTranslatef(posicaoX, posicaoY, 0.0f);
 
-	// Aplica uma rota��o sobre a casinha que ser� desenhada
+    // Aplica uma rota��o sobre a casinha que ser� desenhada
 	glRotatef(angulo, 0.0f, 0.0f, 1.0f);
 
 	// Aplica uma escala sobre a casinha que ser� desenhada
-	glScalef(2.0f, 1.0f, 1.0f);
+	glScalef(escala, escala, 1.0f);
 
 	// Desenha uma casinha composta de um quadrado e um tri�ngulo
 
@@ -47,43 +40,43 @@ void Desenha(void)
 	glColor3f(0.0f, 0.0f, 1.0f);
 	// Desenha a casa
 	glBegin(GL_QUADS);
-	glVertex2f(-15.0f, -15.0f);
-	glVertex2f(-15.0f, 5.0f);
-	glVertex2f(15.0f, 5.0f);
-	glVertex2f(15.0f, -15.0f);
+		glVertex2f(-15.0f,-15.0f);
+		glVertex2f(-15.0f,  5.0f);
+		glVertex2f( 15.0f,  5.0f);
+		glVertex2f( 15.0f,-15.0f);
 	glEnd();
 
 	// Altera a cor do desenho para branco
 	glColor3f(1.0f, 1.0f, 1.0f);
 	// Desenha a porta e a janela
 	glBegin(GL_QUADS);
-	glVertex2f(-4.0f, -14.5f);
-	glVertex2f(-4.0f, 0.0f);
-	glVertex2f(4.0f, 0.0f);
-	glVertex2f(4.0f, -14.5f);
-	glVertex2f(7.0f, -5.0f);
-	glVertex2f(7.0f, -1.0f);
-	glVertex2f(13.0f, -1.0f);
-	glVertex2f(13.0f, -5.0f);
+		glVertex2f(-4.0f,-14.5f);
+		glVertex2f(-4.0f,  0.0f);
+		glVertex2f( 4.0f,  0.0f);
+		glVertex2f( 4.0f,-14.5f);
+		glVertex2f( 7.0f,-5.0f);
+		glVertex2f( 7.0f,-1.0f);
+		glVertex2f(13.0f,-1.0f);
+		glVertex2f(13.0f,-5.0f);
 	glEnd();
 
 	// Altera a cor do desenho para azul
 	glColor3f(0.0f, 0.0f, 1.0f);
 	// Desenha as "linhas" da janela
 	glBegin(GL_LINES);
-	glVertex2f(7.0f, -3.0f);
-	glVertex2f(13.0f, -3.0f);
-	glVertex2f(10.0f, -1.0f);
-	glVertex2f(10.0f, -5.0f);
+		glVertex2f( 7.0f,-3.0f);
+		glVertex2f(13.0f,-3.0f);
+		glVertex2f(10.0f,-1.0f);
+		glVertex2f(10.0f,-5.0f);
 	glEnd();
 
 	// Altera a cor do desenho para vermelho
 	glColor3f(1.0f, 0.0f, 0.0f);
 	// Desenha o telhado
-	glBegin(GL_TRIANGLES);
-	glVertex2f(-15.0f, 5.0f);
-	glVertex2f(0.0f, 17.0f);
-	glVertex2f(15.0f, 5.0f);
+    glBegin(GL_TRIANGLES);
+		glVertex2f(-15.0f, 5.0f);
+		glVertex2f(  0.0f,17.0f);
+		glVertex2f( 15.0f, 5.0f);
 	glEnd();
 
 	// Executa os comandos OpenGL
@@ -96,8 +89,7 @@ void AlteraTamanhoJanela(GLsizei w, GLsizei h)
 	GLsizei largura, altura;
 
 	// Evita a divisao por zero
-	if (h == 0)
-		h = 1;
+	if(h == 0) h = 1;
 
 	// Atualiza as vari�veis
 	largura = w;
@@ -113,35 +105,46 @@ void AlteraTamanhoJanela(GLsizei w, GLsizei h)
 	// Estabelece a janela de sele��o (esquerda, direita, inferior,
 	// superior) mantendo a propor��o com a janela de visualiza��o
 	if (largura <= altura)
-		gluOrtho2D(-40.0f, 40.0f, -40.0f * altura / largura, 40.0f * altura / largura);
+		gluOrtho2D (-40.0f, 40.0f, -40.0f*altura/largura, 40.0f*altura/largura);
 	else
-		gluOrtho2D(-40.0f * largura / altura, 40.0f * largura / altura, -40.0f, 40.0f);
+		gluOrtho2D (-40.0f*largura/altura, 40.0f*largura/altura, -40.0f, 40.0f);
 }
 
-void TeclasEspeciais(int key, int x, int y)
+// Fun��o callback chamada para gerenciar eventos de teclas
+void Teclado (unsigned char key, int x, int y)
+{
+	if (key == 27)
+		exit(0);
+}
+
+void TeclasEspeciais (int key, int x, int y)
 {
 
-	switch (key)
-	{
-	case GLUT_KEY_LEFT:
-		angulo = -90;
-		break;
-	case GLUT_KEY_RIGHT:
-		angulo = 90;
-		break;
-	case GLUT_KEY_UP:
-		angulo = 180;
-		break;
-	case GLUT_KEY_DOWN:
-		angulo = 0;
-		break;
-	}
+    switch (key)
+    {
+        case GLUT_KEY_LEFT: posicaoX--;
+                            break;
+    	case GLUT_KEY_RIGHT: posicaoX++;
+                             break;
+        case GLUT_KEY_UP: posicaoY++;
+                          break;
+        case GLUT_KEY_DOWN: posicaoY--;
+                            break;
+    	case GLUT_KEY_PAGE_UP: angulo++;
+                               break;
+    	case GLUT_KEY_PAGE_DOWN: angulo--;
+                                 break;
+        case GLUT_KEY_HOME: escala++;
+                            break;
+        case GLUT_KEY_END: escala--;
+                           break;
+    }
 
-	Desenha();
+    Desenha();
 }
 
 // Fun��o respons�vel por inicializar par�metros e vari�veis
-void Inicializa(void)
+void Inicializa (void)
 {
 	// Define a cor de fundo da janela de visualiza��o como branca
 	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
@@ -150,20 +153,20 @@ void Inicializa(void)
 // Programa Principal
 int main(int argc, char *argv[])
 {
-	// Utilizada para inicializar a biblioteca GLUT
-	glutInit(&argc, argv);
+    // Utilizada para inicializar a biblioteca GLUT
+    glutInit(&argc, argv);
 
 	// Define do modo de opera��o da GLUT
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
 
 	// Especifica a posi��o inicial da janela GLUT
-	glutInitWindowPosition(5, 5);
+	glutInitWindowPosition(5,5);
 
 	// Especifica o tamanho inicial em pixels da janela GLUT
-	glutInitWindowSize(450, 450);
+	glutInitWindowSize(450,450);
 
 	// Cria a janela passando como argumento o t�tulo da mesma
-	glutCreateWindow("Desenho de uma casa com transla��o, rota��o e escala");
+	glutCreateWindow("Desenho de uma casa com translacao, rotacao e escala");
 
 	// Registra a fun��o callback de redesenho da janela de visualiza��o
 	glutDisplayFunc(Desenha);
@@ -171,6 +174,10 @@ int main(int argc, char *argv[])
 	// Registra a fun��o callback de redimensionamento da janela de visualiza��o
 	glutReshapeFunc(AlteraTamanhoJanela);
 
+	// Registra a fun��o callback para tratamento das teclas ASCII
+	glutKeyboardFunc (Teclado);
+
+	// Registra a fun��o callback para tratamento das teclas Especiais
 	glutSpecialFunc(TeclasEspeciais);
 
 	// Chama a fun��o respons�vel por fazer as inicializa��es
